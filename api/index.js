874f3ba80b60da62daddfc7bc9d90672a4698767
christianlopez4 <christian.lopez4@correo.icesi.edu.co>
2021-01-20 20:31:19 -0500

    Agrego documentación - dummy

--- api/index.js
+++ api/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const config = require('../config.js');
 const user = require('./components/user/network.js');
 const bodyParser = require('body-parser');
+const swaggerUI = require('swagger-ui-express');
+
+const swaggerDoc = require('./swagger.json');
 
 const app = express();
 
 app.use(bodyParser.json()); 
 app.use('/api/user', user);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
     console.log(`API escuchando por en puerto ${config.api.port}`);
